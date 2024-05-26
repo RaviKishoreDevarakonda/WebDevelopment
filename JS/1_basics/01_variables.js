7bc04e9d308c94394a990169ad18b6758594068c
Ravi Kishore Devarakonda <devaravikishore@gmail.com>
2024-05-26 14:05:02 +0530

    Daily Update 26-05-2024

---
+++ JS/1_basics/01_variables.js
@@ -0,0 +1,45 @@
+const accountId=14453
+let accountEmail="ravi@google.com"
+var password="456"
+accountCity="Hyderabad"
+
+//accountId=5 // Constants cannot be changed
+
+
+accountEmail="deva@google.com"
+password="123"
+accountCity="Bangalore"
+let accountState;
+//Print console as table
+
+console.table([accountState,accountId,accountEmail,password,accountCity]);
+
+// Camel Case Declaration, name is easily recognisable and understandable
+
+
+/* JS has some defined Keywords,
+const cannot be changed 
+
+let can be changed
+var can be changed
+for city nothing is declared
+
+
+When we declare a variable a certain amount of memory is reserved and the name
+ is given as the variable name for that memory location
+Variables can be declared using 2 ways
+var and let
+
+We have something called as scope.
+so we use only let to avoid the issues with scope,
+beacause of block scope and functional scope
+
+
+We don't use var
+We can also declare without datatype, but this is not recommended
+But with this method also the memory is reserved.
+
+If it is declared and not defined it is given as undefined
+
+
+*/
---
+++ JS/1_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+// Treat all JS Code as Newer version
+//This is automatically invoked in major cases
+
+//alert(3+3)  //This  usually works in browser , but how to use in Node Env
+//Maintain readable code
